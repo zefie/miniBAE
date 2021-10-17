8aee503ba97b709b87ca39262f217d0a057f6676
Steve Hales <steve@igorlabs.com>
2021-10-16 17:31:15 -0700

    first pass at fixing 64-bit issues

--- minibae/src/BAE_Source/Common/MiniBAE.c
+++ minibae/src/BAE_Source/Common/MiniBAE.c
@@ -6352,7 +6352,7 @@ BAEResult BAESong_LoadRmfFromFile(BAESong song, BAEPathName filePath, short int 
     GM_Song             *pSong;
     OPErr               theErr;
     XLongResourceID     theID;
-    long                size;
+    XDWORD                size;
 
     theErr = NO_ERR;
     if ( (song) && (song->mID == OBJECT_ID) )
