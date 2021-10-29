a11f7b506a7ddef4d967b8da5a354e7133a0ea33
zefie <zefie@zefie.net>
2021-10-29 14:44:36 -0400

    update build_gh_release.sh

--- minibae/build_gh_release.sh
+++ minibae/build_gh_release.sh
@@ -34,10 +34,15 @@ runcmd make -f Makefile clean pack
 install_file "${BDIR}/playbae_linux32_static.gz" "${ODIR}/playbae_linux32_static.gz"
 runcmd make -f Makefile clean
 
+echo "Building Linux 32bit clang..."
+runcmd make -f Makefile.clang clean pack
+install_file "${BDIR}/playbae_linux32_clang_static.gz" "${ODIR}/playbae_linux32_clang_static.gz"
+runcmd make -f Makefile.clang clean
+
 echo "Building MingW32..."
 runcmd make -f Makefile.mingw clean all pack
 install_file "${BDIR}/playbae.exe.gz" "${ODIR}/playbae.exe.gz"
-zip -u "${ODIR}/libMiniBAE_win_DLL.zip" "${BDIR}/"*.dll
+runcmd zip -u "${ODIR}/libMiniBAE_win_DLL.zip" "${BDIR}/"*.dll
 runcmd make -f Makefile.mingw clean
 
 echo "Building Enscripten WASM32..."
