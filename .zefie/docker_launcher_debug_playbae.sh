d55ad3675d8b02af91b2198e3fcbec17361885d0
zefie <zefie@zefie.net>
2026-07-29 19:49:38 -0400

    add a couple of helper scripts

---
+++ .zefie/docker_launcher_debug_playbae.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+THEROOT="$(realpath "$(dirname "${0}")/..")"
+docker run --rm -it -v ${THEROOT}:/src zefie/llvm-mingw:latest .zefie/build-llvm-mingw-static_x86_64_only.sh -DBUILD_CLITOOLS=0 -DBUILD_NBSTUDIO=0 -DBUILD_NBEDITOR=0 -DBUILD_ZEFIDI=0 "${@}"
